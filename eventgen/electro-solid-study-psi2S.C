191f57c98f501353d84fb1a2f2891e5eca047ccd
Gregtom3 <gamatousek@gmail.com>
2021-05-18 10:44:52 -0400

    Tree only saves smeared quantities

--- eventgen/electro-solid-study-psi2S.C
+++ eventgen/electro-solid-study-psi2S.C
@@ -47,54 +47,9 @@ int main(const int argc, const char * argv[]){
   // Set detector
   DETECTOR::SetDetector("SoLID");
   
-  // detected 1
-  TFile * fall1 = new TFile("result-electro-psi2S/Dsolid1.root", "RECREATE");
-  TH1D * hMJpsi1 = new TH1D("Mass_ee_Psi2S", ";M[e^{+}e^{-}] (GeV);Events / hour", 100, 2.6, 3.6);
-  TH2D * hMomentum1 = new TH2D("PePe_Psi2S", ";P[e^{+}] (GeV);P[e^{-}] (GeV)", 100, 0.0, 10.0, 100, 0.0, 10.0);
-  TH2D * hThetaPelectron1 = new TH2D("ThetaP_eminus_Psi2S", ";#theta[e^{-}] (deg);P[e^{-}] (GeV)", 90, 0.0, 180.0, 100, 0.0, 10.0);
-  TH2D * hThetaPpositron1 = new TH2D("ThetaP_eplus_Psi2S", ";#theta[e^{+}] (deg);P[e^{+}] (GeV)", 90, 0.0, 180.0, 100, 0.0, 10.0);
-  TH2D * hThetaPproton1 = new TH2D("ThetaP_proton_Psi2S", ";#theta[p] (deg);P[p] (GeV)", 90, 0.0, 180.0, 100, 0.0, 10.0);
-  TH2D * hAngleP1 = new TH2D("AngleP_Psi2S", ";#alpha<e^{+}e^{-}> (deg);P[e^{+}] (GeV)", 90, 0.0, 180.0, 100, 0.0, 10.0);
-  TH1D * hFermiP1 = new TH1D("FermiP_Psi2S", ";P[N] (GeV);Events / hour", 100, 0.0, 1.0);
-  TH1D * hFermiPz1 = new TH1D("FermiPz_Psi2S", ";Pz[N] (GeV);Events / hour", 100, -1.0, 1.0);
-  TH1D * hPJpsi1 = new TH1D("PPsi2S", ";P[#psi (2S)] (GeV);Events / hour", 100, 0.0, 10.0);
-  TH2D * hFermiPPJpsi1 = new TH2D("FermiPPPsi2S", ";P[N] (GeV);P[#psi (2S)] (GeV)", 100, 0.0, 1.0, 100, 0.0, 10.0);
-  hMJpsi1->SetDirectory(fall1);
-  hMomentum1->SetDirectory(fall1);
-  hThetaPelectron1->SetDirectory(fall1);
-  hThetaPpositron1->SetDirectory(fall1);
-  hThetaPproton1->SetDirectory(fall1);
-  hAngleP1->SetDirectory(fall1);
-  hFermiP1->SetDirectory(fall1);
-  hFermiPz1->SetDirectory(fall1);
-  hPJpsi1->SetDirectory(fall1);
-  hFermiPPJpsi1->SetDirectory(fall1);
-
-  // detected subthreshold 1
-  TFile * fsub1 = new TFile("result-electro-psi2S/Dsolidsub1.root", "RECREATE");
-  TH1D * hMJpsisub1 = new TH1D("Mass_ee_Psi2S", ";M[e^{+}e^{-}] (GeV);Events / hour", 100, 3.0, 4.0);
-  TH2D * hMomentumsub1 = new TH2D("PePe_Psi2S", ";P[e^{+}] (GeV);P[e^{-}] (GeV)", 100, 0.0, 10.0, 100, 0.0, 10.0);
-  TH2D * hThetaPelectronsub1 = new TH2D("ThetaP_eminus_Psi2S", ";#theta[e^{-}] (deg);P[e^{-}] (GeV)", 90, 0.0, 180.0, 100, 0.0, 10.0);
-  TH2D * hThetaPpositronsub1 = new TH2D("ThetaP_eplus_Psi2S", ";#theta[e^{+}] (deg);P[e^{+}] (GeV)", 90, 0.0, 180.0, 100, 0.0, 10.0);
-  TH2D * hThetaPprotonsub1 = new TH2D("ThetaP_proton_Psi2S", ";#theta[p] (deg);P[p] (GeV)", 90, 0.0, 180.0, 100, 0.0, 10.0);
-  TH2D * hAnglePsub1 = new TH2D("AngleP_Psi2S", ";#alpha<e^{+}e^{-}> (deg);P[e^{+}] (GeV)", 90, 0.0, 180.0, 100, 0.0, 10.0);
-  TH1D * hFermiPsub1 = new TH1D("FermiP_Psi2S", ";P[N] (GeV);Events / hour", 100, 0.0, 1.0);
-  TH1D * hFermiPzsub1 = new TH1D("FermiPz_Psi2S", ";Pz[N] (GeV);Events / hour", 100, -1.0, 1.0);
-  TH1D * hPJpsisub1 = new TH1D("PPsi2S", ";P[#psi (2S)] (GeV);Events / hour", 100, 0.0, 10.0);
-  TH2D * hFermiPPJpsisub1 = new TH2D("FermiPPPsi2S", ";P[N] (GeV);P[#psi (2S)] (GeV)", 100, 0.0, 1.0, 100, 0.0, 10.0);
-  hMJpsisub1->SetDirectory(fsub1);
-  hMomentumsub1->SetDirectory(fsub1);
-  hThetaPelectronsub1->SetDirectory(fsub1);
-  hThetaPpositronsub1->SetDirectory(fsub1);
-  hThetaPprotonsub1->SetDirectory(fsub1);
-  hAnglePsub1->SetDirectory(fsub1);
-  hFermiPsub1->SetDirectory(fsub1);
-  hFermiPzsub1->SetDirectory(fsub1);
-  hPJpsisub1->SetDirectory(fsub1);
-  hFermiPPJpsisub1->SetDirectory(fsub1);
 
   // TTree Info
-  TFile * fall2 = new TFile("result-electro-psi2S/Dsolid.root", "RECREATE");
+  TFile * fall2 = new TFile("result-electro-psi2S/Dsolid_electro.root", "RECREATE");
   TTree * tree = new TTree("tree","");
   tree->SetDirectory(fall2);
 
@@ -174,32 +129,7 @@ int main(const int argc, const char * argv[]){
       ePlusOutSmear=(TLorentzVector*)_ePlusOutSmear.Clone();
       eMinusOutSmear=(TLorentzVector*)_eMinusOutSmear.Clone();
 
-      if (CheckAcceptance(kf[0], 6.0, 22.0) * CheckAcceptance(kf[2], 6.0, 22.0) * CheckAcceptance(kf[3], 6.0, 22.0)){
-	hMJpsi1->Fill( (kf[2]+kf[3]).M(), weight);
-	hMomentum1->Fill( kf[2].P(), kf[3].P(), weight);
-	hThetaPelectron1->Fill( kf[3].Theta()/M_PI*180, kf[3].P(), weight);
-	hThetaPpositron1->Fill( kf[2].Theta()/M_PI*180, kf[2].P(), weight);
-	hThetaPproton1->Fill( kf[1].Theta()/M_PI*180, kf[1].P(), weight);
-	hAngleP1->Fill( kf[3].Angle(kf[2].Vect())/M_PI*180, kf[2].P(), weight);
-	hFermiP1->Fill( ki[1].P(), weight);
-	hFermiPz1->Fill( ki[1].Pz(), weight);
-	hPJpsi1->Fill( (kf[2]+kf[3]).P(), weight);
-	hFermiPPJpsi1->Fill( ki[1].P(), (kf[2]+kf[3]).P(), weight);
-	
-	if (q.E() < Mjpsi + (Mjpsi * Mjpsi - q * q) / (2.0 * Mp)){
-	  hMJpsisub1->Fill( (kf[2]+kf[3]).M(), weight);
-	  hMomentumsub1->Fill( kf[2].P(), kf[3].P(), weight);
-	  hThetaPelectronsub1->Fill( kf[3].Theta()/M_PI*180, kf[3].P(), weight);
-	  hThetaPpositronsub1->Fill( kf[2].Theta()/M_PI*180, kf[2].P(), weight);
-	  hThetaPprotonsub1->Fill( kf[1].Theta()/M_PI*180, kf[1].P(), weight);
-	  hAnglePsub1->Fill( kf[3].Angle(kf[2].Vect())/M_PI*180, kf[2].P(), weight);
-	  hFermiPsub1->Fill( ki[1].P(), weight);
-	  hFermiPzsub1->Fill( ki[1].Pz(), weight);
-	  hPJpsisub1->Fill( (kf[2]+kf[3]).P(), weight);
-	  hFermiPPJpsisub1->Fill( ki[1].P(), (kf[2]+kf[3]).P(), weight);
-	}
-      }
-
+     
       // Filling in TTree
 
       // Calculate if the process was "subthreshold"
@@ -222,40 +152,14 @@ int main(const int argc, const char * argv[]){
       Eg_smear = CalcEg(_ePlusOutSmear+_eMinusOutSmear+_pOutSmear);
       vmSmear=_ePlusOutSmear+_eMinusOutSmear;
 
-
-      tree->Fill();
+      if(weight_smear2>0.0)
+	tree->Fill();
       
     }
   }
   cout << count << endl;
   GENERATE::print_fail();
-  //1
-  hMJpsi1->Scale(lumi*time/Nsim);
-  hMomentum1->Scale(lumi*time/Nsim);
-  hThetaPelectron1->Scale(lumi*time/Nsim);
-  hThetaPpositron1->Scale(lumi*time/Nsim);
-  hThetaPproton1->Scale(lumi*time/Nsim);
-  hAngleP1->Scale(lumi*time/Nsim);
-  hFermiP1->Scale(lumi*time/Nsim);
-  hFermiPz1->Scale(lumi*time/Nsim);
-  hPJpsi1->Scale(lumi*time/Nsim);
-  hFermiPPJpsi1->Scale(lumi*time/Nsim);
-  fall1->Write();
-  fall1->Close();
-
-  hMJpsisub1->Scale(lumi*time/Nsim);
-  hMomentumsub1->Scale(lumi*time/Nsim);
-  hThetaPelectronsub1->Scale(lumi*time/Nsim);
-  hThetaPpositronsub1->Scale(lumi*time/Nsim);
-  hThetaPprotonsub1->Scale(lumi*time/Nsim);
-  hAnglePsub1->Scale(lumi*time/Nsim);
-  hFermiPsub1->Scale(lumi*time/Nsim);
-  hFermiPzsub1->Scale(lumi*time/Nsim);
-  hPJpsisub1->Scale(lumi*time/Nsim);
-  hFermiPPJpsisub1->Scale(lumi*time/Nsim);
-  fsub1->Write();
-  fsub1->Close();
-
+ 
 
   //2
   fall2->Write();
