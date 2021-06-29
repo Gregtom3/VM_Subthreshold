f2859fbf3204886fb6f6dd942d7f8b61095ece4d
GregoryMatousek <gregory.matousek@stonybrook.edu>
2021-06-29 14:45:37 -0400

    Plotting macros

--- psi2S-eventgen/plotting_macros/plot_nevents_vs_Egamma.C
+++ psi2S-eventgen/plotting_macros/plot_nevents_vs_Egamma.C
@@ -2,7 +2,7 @@ int plot_nevents_vs_Egamma()
 {
   int A = 1;
   //  int type = 0; // 0 --> photo , 1 --> electro
-  double eIn_E = 11.0;
+  double eIn_E = 17.0;
   
   // ************************************************************* //
 
@@ -39,11 +39,23 @@ int plot_nevents_vs_Egamma()
   TH1F *h_2 = new TH1F("h_2",";E_{#gamma} [GeV];Nevents",nbins_x,xmin,xmax);
   TH1F *h_3 = new TH1F("h_3",";E_{#gamma} [GeV];Nevents",nbins_x,xmin,xmax);
 
+  TH1F *h_sub_1 = new TH1F("h_sub_1",";E_{#gamma} [GeV];Nevents",nbins_x,xmin,xmax);
+  TH1F *h_sub_2 = new TH1F("h_sub_2",";E_{#gamma} [GeV];Nevents",nbins_x,xmin,xmax);
+  TH1F *h_sub_3 = new TH1F("h_sub_3",";E_{#gamma} [GeV];Nevents",nbins_x,xmin,xmax);
+
   const double histmin = 1;
   const double histmax = 9.5*pow(10,4);
   h_1->GetYaxis()->SetRangeUser(histmin,histmax);
   h_2->GetYaxis()->SetRangeUser(histmin,histmax);
   h_1->GetYaxis()->SetTitleOffset(0.95);
+
+  h_sub_1->SetFillColor(kRed);
+  h_sub_2->SetFillColor(kRed);
+  h_sub_3->SetFillColor(kRed);
+
+  h_sub_1->SetFillStyle(3001);
+  h_sub_2->SetFillStyle(3001);
+  h_sub_3->SetFillStyle(3001);
 
   // ************************************************************* //
 
@@ -62,15 +74,26 @@ int plot_nevents_vs_Egamma()
   c->cd(1);
   tIn1->Draw("gamma->E()>>h_1","weight_smear2","hist");
   latex.DrawLatexNDC(0.15,0.95,Form("#bf{SoLID} Simulation (%.1f GeV beam)",eIn_E));
-  latex.DrawLatexNDC(0.15,0.9,Form("e+%s #rightarrow e'+p'+#psi(2S) (e^{-}e^{+}) , Photoprodution",stringA.Data()));
+  latex.DrawLatexNDC(0.15,0.9,Form("e+%s #rightarrow e'+p'+#psi(2S) (e^{-}e^{+}) , #bf{Photoprodution}",stringA.Data()));
   latex.DrawLatexNDC(0.15,0.85,Form("50 days at 1.2e37 cm^{-2}s^{-1}A^{-1} #rightarrow %.0f events",h_1->Integral()));
+  if(A!=1)
+    {
+      tIn1->Draw("gamma->E()>>h_sub_1","weight_smear2*is_sub","hist same");
+      latex.DrawLatexNDC(0.15,0.8,Form("#color[2]{%.0f events subthreshold}",h_sub_1->Integral()));
+    }
+  gPad->RedrawAxis();
 
   c->cd(2);
   tIn2->Draw("gamma->E()>>h_2","weight_smear2","hist");
   latex.DrawLatexNDC(0.1,0.95,Form("#bf{SoLID} Simulation (%.1f GeV beam)",eIn_E));
-  latex.DrawLatexNDC(0.1,0.9,Form("e+%s #rightarrow e'+p'+#psi(2S) (e^{-}e^{+}) , Electroprodution",stringA.Data()));
+  latex.DrawLatexNDC(0.1,0.9,Form("e+%s #rightarrow e'+p'+#psi(2S) (e^{-}e^{+}) , #bf{Electroprodution}",stringA.Data()));
   latex.DrawLatexNDC(0.1,0.85,Form("50 days at 1.2e37 cm^{-2}s^{-1}A^{-1} #rightarrow %.0f events",h_2->Integral()));
-
+  if(A!=1)
+    {
+      tIn2->Draw("gamma->E()>>h_sub_2","weight_smear2*is_sub","hist same");
+      latex.DrawLatexNDC(0.1,0.8,Form("#color[2]{%.0f events subthreshold}",h_sub_2->Integral()));
+    }
+  gPad->RedrawAxis();
   
   c->cd(3);
   //  gPad->SetRightMargin(0.1);
@@ -78,9 +101,15 @@ int plot_nevents_vs_Egamma()
   h_3->Draw("hist");
   h_3->GetYaxis()->SetRangeUser(histmin,histmax);  
   latex.DrawLatexNDC(0.1,0.95,Form("#bf{SoLID} Simulation (%.1f GeV beam)",eIn_E));
-  latex.DrawLatexNDC(0.1,0.9,Form("e+%s #rightarrow e'+p'+#psi(2S) (e^{-}e^{+}) , Photo+electro prodution",stringA.Data()));
+  latex.DrawLatexNDC(0.1,0.9,Form("e+%s #rightarrow e'+p'+#psi(2S) (e^{-}e^{+}) , #bf{Photo+electro prodution}",stringA.Data()));
   latex.DrawLatexNDC(0.1,0.85,Form("50 days at 1.2e37 cm^{-2}s^{-1}A^{-1} #rightarrow %.0f events",h_3->Integral()));
-
+  h_sub_3->Add(h_sub_1,h_sub_2);
+  if(A!=1)
+    {
+      h_sub_3->Draw("hist same");
+      latex.DrawLatexNDC(0.1,0.8,Form("#color[2]{%.0f events subthreshold}",h_sub_3->Integral()));
+    }
+  gPad->RedrawAxis();
 
   return 0;
 }
